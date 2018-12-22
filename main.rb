3861d38efa92153f307176469fbc7a80aaf7f409
Felipe Menelau <naffowna@yahoo.com.br>
2018-12-22 16:07:23 -0300

    Adiciona comentários sobre criação do grafo

--- main.rb
+++ main.rb
@@ -2,6 +2,8 @@ require_relative 'graph'
 
 def main
   graph = Graph.new
+
+  # Nós no grafo são adcionados ao adicionar arestas
   graph.add(1, 2)
   graph.add(2, 6)
   graph.add(6, 7)
@@ -10,6 +12,8 @@ def main
   graph.add(5, 7)
   graph.add(3, 5)
   graph.add(0, 3)
+
+  # Nós sem arestas devem apontar para nil
   graph.add(7, nil)
 
   graph.dfs_topological_sort
